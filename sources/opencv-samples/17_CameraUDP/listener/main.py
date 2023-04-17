5a035c58de343c1cdacd4eb09d8be7883a7841b5
RomanCLV <roman.clavier.2001@gmail.com>
2023-04-17 21:15:12 +0200

    update send and received udp python

--- sources/opencv-samples/17_CameraUDP/listener/main.py
+++ sources/opencv-samples/17_CameraUDP/listener/main.py
@@ -1,116 +1,127 @@
+import argparse
 import cv2 as cv
 import numpy as np
 import socket
-import argparse
+import struct
+import sys
+import os
 import signal
-
-
-global has_to_break
 
 
 fps_choices = [1, 5, 10, 12, 15, 20, 24, 25, 29.97, 30, 48, 50, 59.94, 60, 120, 144, 240, 300]
 
 
-def sigint_handler(signal, frame):
-    global has_to_break
-    has_to_break = True
+def recv_frame_whole(sock):
+    try:
+        data, addr = sock.recvfrom(65535)
+        frame = np.frombuffer(data, dtype=np.uint8)
+        return cv.imdecode(frame, cv.IMREAD_COLOR)
+    except socket.timeout:
+        return None
 
 
-def listen_frame(sock, max_size = 65507):
-    if max_size > 65507 or max_size < 1:
-        raise ValueError(f"max_size must be between 1 and 65507. Given {max_size}")
-        
-    # Receive the encoded frame via UDP
-    encoded_frame, address = sock.recvfrom(max_size)
+def recv_frame_sliced(sock):
+    num_packets, packet_data = 0, bytearray()
 
-    # Decode the frame from a byte string
-    return cv.imdecode(np.frombuffer(encoded_frame, dtype=np.uint8), cv.IMREAD_COLOR)
-
-
-def listen_frame_splitted(sock, max_size = 65507):
-    if max_size > 65507 or max_size < 1:
-        raise ValueError(f"max_size must be between 1 and 65507. Given {max_size}")
-        
-    # Réception et reconstruction de l'image à partir des morceaux reçus
-    img_bytes = b''
-    n_chunks = None
-    i = 0
     while True:
-        data, addr = sock.recvfrom(max_size + 4)                            # On reçoit un message UDP
-        seq_num, total_chunks = np.frombuffer(data[:4], dtype=np.uint16)    # On lit l'en-tête
-        chunk = data[4:]
-        if n_chunks is None:
-            n_chunks = total_chunks
-            img_bytes = b'\x00' * (n_chunks * max_size)                     # On initialise le buffer pour l'image complète
-        img_bytes[seq_num * max_size:(seq_num + 1) * max_size] = chunk      # On ajoute le morceau au buffer
-        i += 1
-        if i == n_chunks:
-            break
+        try:
+            data, addr = sock.recvfrom(65535)
+            num, chunk = struct.unpack('!II', data[:8]), data[8:]
+            num_packets = max(num_packets, num)
 
-    # Convertir les octets en image OpenCV
-    return cv.imdecode(np.frombuffer(img_bytes, dtype=np.uint8), cv.IMREAD_COLOR)
+            packet_data.extend(chunk)
+
+            if len(packet_data) >= num_packets * (len(data) - 8):
+                break
+        except socket.timeout:
+            return None
+
+    frame = np.frombuffer(packet_data, dtype=np.uint8)
+    return cv.imdecode(frame, cv.IMREAD_COLOR)
 
 
-def main():
-    global has_to_break
-    has_to_break = False
+def main(args):
 
+    udp_ip = args.ip
+    udp_port = args.port
+
+    display = False
     write = False
-    video_fps = 30
+    video = None
+    frame_fps = 30
+    window_name = "window"
 
-    args = parser.parse_args()
-    
+    if args.display:
+        display = True
+
+    if args.fps:
+        frame_fps = args.fps
+
     if args.output:
         write = True
-    
-    if args.fps:
-        video_fps = args.fps
-    
-    video = None
+        file_extension = os.path.splitext(args.output)[1]
+        if file_extension.lower() != ".avi":
+            print("Error : the video file extension must be .avi")
+            sys.exit(1)
 
-    # Configure the socket for UDP
-    UDP_IP   = args.udp_address  # IP address of the Raspberry Pi on the same network
-    UDP_PORT = args.udp_port     # listened port
+    window_name = f"Received from {udp_ip}:{udp_port}"
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
+    sock.bind((udp_ip, udp_port))
+    # Définir un délai d'attente (timeout) pour le socket en secondes
+    sock.settimeout(1.0)
 
-    window_name = f"Stream from {UDP_IP}:{UDP_PORT}"
-    cv.namedWindow(window_name)
-    k = 0
-    
-    while True:
-        
-        # Recoie la frame en entier.
-        # frame = listen_frame(sock)
-        
-        # Recoie une frame découpée
-        frame = listen_frame_splitted(sock)
-        
-        if write:
-            if video is None:
-                frame_height, frame_width, frame_channels = frame.shape
-                video = video = cv.VideoWriter(args.output, cv.VideoWriter_fourcc('M','J','P','G'), video_fps, (frame_width, frame_height))
-            video.write(frame)
+    if display:
+        cv.namedWindow(window_name, cv.WINDOW_NORMAL)
+        cv.waitKey(500)
+
+    try:
+        while True:
+            #if args.receive_split:
+            #    frame = recv_frame_sliced(sock)
+            #else:
+            #    frame = recv_frame_whole(sock)
+            frame = recv_frame_sliced(sock)
+
+            if frame is None:
+                print("No frame received")
+                cv.waitKey(500)
+                continue
+
+            if write:
+                if video is None:
+                    fourcc = cv.VideoWriter_fourcc(*'XVID')
+                    video = cv.VideoWriter(args.output, fourcc, frame_fps, (frame.shape[1], frame.shape[0]))
+                video.write(frame)
+
+            if display:
+                cv.imshow(namedWindow, frame)
             
-        # Display the frame
-        cv.imshow(window_name, frame)
-        k = cv.waitKey(1) & 0xFF
-        
-        
-        if has_to_break or k == 27:
-            break
+            if cv.waitKey(1) & 0xFF == 27:
+                break
+                
+    except KeyboardInterrupt:
+        pass
 
-    # Release the resources
-    cv.destroyAllWindows()
+    finally:
+        if video:
+            video.release()
+
+        if display:
+            cv.destroyAllWindows()
+
+        sock.close()
 
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, sigint_handler)
-    parser = argparse.ArgumentParser(description="Camera Capture UDP - Receiver main.py CLI")
-    parser.add_argument("udp_address", type=str, help="udp address to listen")
-    parser.add_argument("udp_port", type=int, help="udp port to listen")
-    parser.add_argument("-o", "--output", type=str, help="write a video file")
-    parser.add_argument("-fps" "--fps", type=float, choices=fps_choices, help="fps of the output video")
-    
-    main()
+    parser = argparse.ArgumentParser(description='Receive video frames via UDP.')
+    parser.add_argument('ip', type=str, help='IP address to bind to')
+    parser.add_argument('port', type=int, help='Port to bind to')
+    parser.add_argument('-o', '--output', type=str, help='Path to save the video file')
+    parser.add_argument('-d', '--display', action='store_true', help='Display received frames in a window')
+    #parser.add_argument('-s', '--receive_split', action='store_true', help='Receive frames split into smaller packets')
+    parser.add_argument('-fps', '--fps', type=int, default=30, fps_choices=fps_choices, help='Frames per second for the video (default: 30)')
 
+    args = parser.parse_args()
+
+    main(args)
--- sources/opencv-samples/17_CameraUDP/sender/main.py
+++ sources/opencv-samples/17_CameraUDP/sender/main.py
@@ -1,127 +1,118 @@
-# https://learnopencv.com/read-write-and-display-a-video-using-opencv-cpp-python/
-
-import cv2 as cv
 import argparse
-import sys
-import signal
+import cv2 as cv
+import numpy as np
 import socket
+import struct
+import sys
+import os
+import signal
 
 
-global has_to_break
+def send_frame_udp(frame, address, sock, max_packet_size=65535):
+    data = cv.imencode('.jpg', frame)[1].tobytes()
+    num_packets = len(data) // max_packet_size + 1
+
+    for i in range(num_packets):
+        packet_data = data[i * max_packet_size: (i + 1) * max_packet_size]
+        packet = struct.pack('!I', i) + packet_data
+        sock.sendto(packet, address)
 
 
-def sigint_handler(signal, frame):
-    global has_to_break
-    has_to_break = True
+def send_frame_udp_split(frame, address, sock, max_packet_size=65535):
+    data = cv.imencode('.jpg', frame)[1].tobytes()
+    num_packets = len(data) // max_packet_size + 1
+
+    for i in range(num_packets):
+        packet_data = data[i * max_packet_size: (i + 1) * max_packet_size]
+        packet = struct.pack('!II', num_packets, i) + packet_data
+        sock.sendto(packet, address)
 
 
-def send_frame(sock, udp_ip, udp_port, frame):
-    # Encode the frame as a byte string
-    encoded_frame = cv.imencode('.jpg', frame)[1].tobytes()
-
-    # Send the encoded frame via UDP
-    sock.sendto(encoded_frame, (udp_ip, udp_port))
-
-
-def send_frame_splitted(sock, udp_ip, udp_port, frame, max_size=65507):
-    if max_size > 65507 or max_size < 1:
-        raise ValueError(f"max_size must be between 1 and 65507. Given {max_size}")
-        
-    # Diviser l'image en morceaux de n octets (la taille maximale autorisée pour les datagrammes UDP)    
-    frame_bytes = frame.tobytes()
-    n_chunks = len(frame_bytes) // max_size + 1
-    chunks = [frame_bytes[i * max_size:(i + 1) * max_size] for i in range(n_chunks)]
-
-    # Envoyer chaque morceau par UDP
-    for i, chunk in enumerate(chunks):
-        message = np.array([i, n_chunks], dtype=np.uint16).tobytes() + chunk
-        sock.sendto(message, (udp_ip, udp_ip))
-
-
-def main():
-    global has_to_break
-    has_to_break = False
+def main(args):
     
-    display = False
-    write = False
+    udp_ip = args.ip
+    udp_port = args.port
 
-    args = parser.parse_args()
+    write = False
+    display = False
+    video = None
 
     if args.display:
         display = True
-        
+
     if args.output:
         write = True
-
-    # Configure the socket for UDP
-    UDP_IP   = args.udp_address # IP address of the PC on the same network
-    UDP_PORT = args.udp_port    # port of the PC on the same network
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        file_extension = os.path.splitext(args.output)[1]
+        if file_extension.lower() != ".avi":
+            print("Error : the video file extension must be .avi")
+            sys.exit(1)
 
     print("Trying to open /dev/video0 with CAP_V4L2")
     cap = cv.VideoCapture(0, cv.CAP_V4L2)  # Add cv::CAP_V4L2 to fix: Embedded video playback halted; module v4l2src0 reported: Failed to allocate required memory.
 
     if not cap.isOpened():
         sys.exit("Can not read the device")
-    
-    video = None
+
     if write:
+        fourcc = cv.VideoWriter_fourcc(*'XVID')
         frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
         frame_fps = cap.get(cv.CAP_PROP_FPS) 
-            
-        video = cv.VideoWriter(args.output, cv.VideoWriter_fourcc('M','J','P','G'), frame_fps, (frame_width, frame_height))
+        video = cv.VideoWriter(args.output, fourcc, frame_fps, (frame_width, frame_height))
 
-
-    print("Press ESC to quit")
-    window_name = "Camera Capture"
-    
+    window_name = f"Send to {udp_ip}:{udp_port}"
     if display:
-        cv.namedWindow(window_name)
+        cv.namedWindow(window_name, cv.WINDOW_NORMAL)
         cv.waitKey(500)
-    k = 0
 
-    while True:
-          
-        ret, frame = cap.read()
-        
-        if not (ret == False or frame is None):
-            if display:
-                cv.imshow(window_name, frame)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-            # Envoie la frame entiere : Erreur UDP : MessageTooLong
-            # send_frame(sock, UDP_IP, UDP_PORT, frame)
-            
-            send_frame_splitted(sock, UDP_IP, UDP_PORT, frame)
-            
+    try:
+        while True:
+            ret, frame = cap.read()
+
+            if not ret:
+                print("frame is empty")
+                cv.waitKey(500)
+                continue
+                
             if write:
                 video.write(frame)
 
-            k = cv.waitKey(1) & 0xFF
+            # if args.send_split:
+            #     send_frame_udp_split(frame, (udp_ip, udp_port), sock)
+            # else:
+            #     send_frame_udp(frame, (udp_ip, udp_port), sock)
+            send_frame_udp_split(frame, (udp_ip, udp_port), sock)
 
-        else:
-            print("frame is empty")
-            k = cv.waitKey(500) & 0xFF
+            if display:
+                cv.imshow(window_name, frame)
+            
+            if cv.waitKey(1) & 0xFF == 27:  # 27 is the ESC key
+                break
 
-        # break the loop if Ctrl+C or key ESC
-        if has_to_break or k == 27:
-            break
+    except KeyboardInterrupt:
+        pass
 
-    print("Capture done")
-    # After the loop release the cap object
-    cap.release()
-    if video:
-        video.release()
+    finally:
+        cap.release()
 
-    # Destroy all the windows
-    cv.destroyAllWindows()
+        if video:
+            video.release()
 
+        if display:
+            cv.destroyAllWindows()
+
+        sock.close()
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, sigint_handler)
-    parser = argparse.ArgumentParser(description="Camera Capture UDP - Sender main.py CLI")
-    parser.add_argument("udp_address", type=str, help="udp address to send")
-    parser.add_argument("udp_port", type=int, help="udp port to send")
-    parser.add_argument("-d", "--display", action="store_true", help="display a window of the camera")
-    parser.add_argument("-o", "--output", type=str, help="write a video file")
-    main()
+    parser = argparse.ArgumentParser(description='Stream video frames via UDP.')
+    parser.add_argument('ip', type=str, help='Destination IP address')
+    parser.add_argument('port', type=int, help='Destination port')
+    parser.add_argument('-o', '--output', type=str, help='Path to save the video file')
+    parser.add_argument('-d', '--display', action='store_true', help='Display video frames in a window')
+    # parser.add_argument('-s', '--send_split', action='store_true', help='Send frames split into smaller packets')
+
+    args = parser.parse_args()
+
+    main(args)
